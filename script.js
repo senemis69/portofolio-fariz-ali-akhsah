fedb369cb8a3fdee1377417f01a8319b86702a27
senemis69 <149548660+senemis69@users.noreply.github.com>
2026-03-04 22:28:31 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,34 @@ const heroBg = document.querySelector(".hero-bg");
 window.addEventListener("scroll", () => {
 
 let scroll = window.scrollY;
-
 let scale = 1 + scroll * 0.0004;
 
 heroBg.style.transform = `scale(${scale})`;
 
 });
+
+const open = document.getElementById("openCV");
+const popup = document.getElementById("cvPopup");
+const close = document.getElementById("closeCV");
+
+open.onclick = () => {
+
+popup.style.display = "flex";
+
+}
+
+close.onclick = () => {
+
+popup.style.display = "none";
+
+}
+
+window.onclick = (e)=>{
+
+if(e.target == popup){
+
+popup.style.display = "none";
+
+}
+
+}
